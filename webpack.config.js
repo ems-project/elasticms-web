d56214c3f70f1e77995196e78c29c72696a7370a
David Mattei <david.mattei@smals.be>
2018-11-05 11:59:06 +0100

    update clientHelperBundle 3.0.8

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var Encore = require('@symfony/webpack-encore');
 var path = require('path');
 
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 Encore
     .setOutputPath('public/bundles/emsch_assets')
     .setPublicPath('/bundles/emsch_assets')
@@ -9,6 +11,10 @@ Encore
     .cleanupOutputBeforeBuild()
     .autoProvidejQuery()
     .enablePostCssLoader()
+    .autoProvideVariables({
+        'window.jQuery': 'jquery',
+        'window.$': 'jquery'
+    })
     .enableSourceMaps(!Encore.isProduction())
     .enableSassLoader(function(sassOptions) {}, {
         resolveUrlLoader: false
@@ -16,9 +22,20 @@ Encore
 
     .addStyleEntry('css/app', './assets/css/app.scss')
     .addEntry('js/app', './assets/js/app.js')
+    .addPlugin(new CopyWebpackPlugin([
+        // copies to {output}/static
+        { from: './assets/static', to: 'static' }
+    ]))
 ;
 
 var config = Encore.getWebpackConfig();
 config.resolve.alias.emsch = path.resolve(__dirname, 'vendor/elasticms/client-helper-bundle/Resources/assets/AdminMenu/app.js');
+
+//disable amd loader
+config.module.rules.unshift({
+    parser: {
+        amd: false,
+    }
+});
 
 module.exports = config;
