775f9e009308f85978daf3984b784e2e6e71356e
David Mattei <david.mattei@smals.be>
2021-01-13 09:31:49 +0100

    fix: deprecation remove sf WebserverBundle
    User Deprecated: Using the WebserverBundle is deprecated since Symfony 4.4. The new Symfony local server has more features, you can use it instead.

--- config/bundles.php
+++ config/bundles.php
@@ -13,7 +13,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     EMS\ClientHelperBundle\EMSClientHelperBundle::class => ['all' => true],
     EMS\CommonBundle\EMSCommonBundle::class => ['all' => true],
     EMS\FormBundle\EMSFormBundle::class => ['all' => true],
